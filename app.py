814ae5e6de63b8eb032809f2178c806416e40683
Max <maxwellhill2004@icloud.com>
2025-03-26 15:33:16 +0000

    yesyes

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ import numpy as np
 files = (os.listdir(r'C:\Users\Max\Downloads\N2'))
 testEpoch = data_io.raw.epochtoRawArray(r'C:\Users\Max\Downloads\N2\bdc14_Z4_0253.mat')
 coh = features_io.features.coh(testEpoch)
+# how bad would it be to pilot the main program with a numpy array, and when using 
+# feature selection or visualsation
 #visualization.plot.raw_plot(testEpoch)
 #visualization.plot.raw_sensors(testEpoch)
--- features_io/features.py
+++ features_io/features.py
@@ -8,13 +8,12 @@ def coh(data):
     events = mne.make_fixed_length_events(data, duration=8, overlap=0.0)
     epochs = mne.Epochs(data, events, tmin=0, tmax = 8, baseline=None, preload=True )
     con = sp(method = 'coh', data=epochs, faverage=False)
-    print(con.get_data(output='dense').shape)
+    print(epochs.info)
     plot_connectivity_circle(
         con.get_data(output='dense')[:, :, 0],  # Shape: (19, 19)
         node_names=epochs.info['ch_names'],
         title='wPLI in Alpha Band',
         facecolor='white',
-        colormap='viridis'
-    ) 
-    # in mmy head it probably makes the most sense to direcly pass numpy arrays about ?
+        colormap='viridis',
+        show=True)
     return(con)
